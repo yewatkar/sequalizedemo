26d5317cb1d1379067e9383099760883f209e3d2
yewatkar <ashwiniyewatkar07@gmail.com>
2023-03-23 16:55:33 +0530

    index file changed

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 module.exports = (app)=>{
   router.get('/', (req, res)=>{
-    res.send({ title: 'Express' });
+    res.send({ title: 'sequalize demo' });
   })
   router.post('/', (req, res)=>{
-    res.send({ title: 'Express' });
+    res.send({ title: 'sequalize demo' });
   })
 
 
